37e32bc19ad9b5746b663c590bb700ac9e6d5443
Pavel Ponec <pponec@gmail.com>
2021-01-13 17:11:58 +0100

    WEB: The method Element.setAttribute() have got a new shortcut: Element.setAttr()

--- runtests-final.sh
+++ runtests-final.sh
@@ -4,17 +4,12 @@
 # Install the ApacheBench:
 ab -V || (echo Install ApacheBench for Ubuntu && sudo apt-get install apache2)
 
-myArray="groovy jsp freemarker velocity thymeleaf jade scalate mustache pebble handlebars jtwig httl chunk htmlFlow trimou rocker ickenham rythm liqp kotlinx ujorm"
-echo "$myArray" | tr ' ' '\n' | while read item; do
-        echo Warming up: $item
-        result=`ab -q -n 25000 -c 25 -s 120 http://localhost:8080/$item | grep "Time taken for tests"`
-done
-
 for i in $(seq 1 10); do
-    echo "=== An iteration: #$i"
+    echo "### An iteration: #$i [$(date +%Y-%m-%d,%H:%M:%S)]"
     echo "$myArray" | tr ' ' '\n' | while read item; do
         result=`ab -q -n 25000 -c 25 -s 120 http://localhost:8080/$item | grep "Time taken for tests"`
         echo $item $result
     done
 done
+echo "### Done [$(date +%Y-%m-%d,%H:%M:%S)]"
 
--- runtests.sh
+++ runtests.sh
@@ -5,7 +5,7 @@ ab -V || (echo Install ApacheBench for Ubuntu && sudo apt-get install apache2)
 
 myArray="groovy jsp freemarker velocity thymeleaf jade scalate mustache pebble handlebars jtwig httl chunk htmlFlow trimou rocker ickenham rythm liqp kotlinx ujorm"
 echo "$myArray" | tr ' ' '\n' | while read item; do
-  result=`ab -q -n 100 -c 10 -s 120 http://localhost:8080/$item | grep "Time taken for tests"`
-  echo $item $result
+    result=`ab -q -n 100 -c 10 -s 120 http://localhost:8080/$item | grep "Time taken for tests"`
+    echo $item $result
 done
 
